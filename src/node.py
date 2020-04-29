682d210446cae4a9550d177547b9952f58829d71
baggakunal <baggakunal93@gmail.com>
2020-04-30 02:56:08 +0530

    moved code to node module

--- src/node.py
+++ src/node.py
@@ -1,6 +1,27 @@
 
 class Node:
-
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+def print_nodes(node: Node):
+    while node is not None:
+        print(node.value)
+        node = node.next
+
+
+def main():
+    first = Node(3)
+
+    middle = Node(5)
+    first.next = middle
+
+    last = Node(7)
+    middle.next = last
+
+    print_nodes(first)
+
+
+if __name__ == '__main__':
+    main()
