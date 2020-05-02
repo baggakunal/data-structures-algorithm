3a40706ec4df27446047523f9744153ca707a74e
baggakunal <baggakunal93@gmail.com>
2020-05-03 05:05:34 +0530

    fixed code

--- project/data_structures/list_based_queue.py
+++ project/data_structures/list_based_queue.py
@@ -2,18 +2,18 @@ class Queue:
     """A First In First Out (FIFO) collection implemented using list."""
     def __init__(self):
         self._list = list()
-        self._size = 0
-        self._head = 0
-        self._tail = -1
+        self._size = 0      # The number of items in the queue
+        self._head = 0      # The index of the first (oldest) item in the queue
+        self._tail = -1     # The index of the last (newest) item in the queue
 
     def _double_size(self):
         if len(self._list) == 0:
             self._list = 4 * [None]
         else:
-            if self._head == 0:
-                new_list = self._list[self._head:]
-            else:
+            if self._tail < self._head:
                 new_list = self._list[self._head:] + self._list[:self._head]
+            else:
+                new_list = self._list[self._head:]
             new_list += self._size * [None]
 
             self._list = new_list
@@ -29,6 +29,7 @@ class Queue:
         if len(self._list) == self._size:
             self._double_size()
 
+        # If tail is at the end of the array, we need to wrap around
         if self._tail == len(self._list) - 1:
             self._tail = 0
         else:
@@ -43,18 +44,18 @@ class Queue:
         :return: The front item from the queue.
         :raises IndexError: If no items are present.
         """
-        if self._head == 0 and self._list[self._head] is None:
-            raise IndexError("The stack is empty.")
+        if self._size == 0:
+            raise IndexError("The queue is empty.")
 
         item = self._list[self._head]
         self._list[self._head] = None
         self._size -= 1
 
-        if self._head == self._tail:
+        if self._head == self._tail:                # If head is at the same index as tail, move both to 0 index
             self._head = self._tail = 0
-        elif self._head == len(self._list) - 1:
+        elif self._head == len(self._list) - 1:     # If head is at the last index in the array, wrap around
             self._head = 0
-        else:
+        else:                                       # Mode to the next value
             self._head += 1
 
         return item
@@ -65,8 +66,8 @@ class Queue:
         :return: The front item in the queue.
         :raises IndexError: If no items are present.
         """
-        if self._head == 0 and self._list[self._head] is None:
-            raise IndexError("The stack is empty.")
+        if self._size == 0:
+            raise IndexError("The queue is empty.")
 
         return self._list[self._head]
 
@@ -89,10 +90,13 @@ class Queue:
 
         :return: The FIFO enumerator.
         """
-        if self._head == 0:
-            print_list = self._list[self._head:]
+        if self._size == 0:
+            return []
+
+        if self._tail < self._head:
+            print_list = self._list[self._head:] + self._list[:self._tail + 1]
         else:
-            print_list = self._list[self._head:] + self._list[:self._head]
+            print_list = self._list[self._head:self._tail + 1]
 
         for item in print_list:
             yield item
